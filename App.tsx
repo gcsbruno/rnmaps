0c2673f2c025578daaddfd0bcdbd276eaab7fa5a
gcsbruno <brunoguimaraes.cs@gmail.com>
2023-02-28 12:30:34 -0300

    init rnmaps with expo location

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react'
+import { View } from 'react-native';
+import { styles } from './styles';
+import MapView from 'react-native-maps';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject
+} from 'expo-location';
+
 
 export default function App() {
+
+  const [location, setLocation] = useState<LocationObject | null>(null)
+
+  async function requestLocationPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition)
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        location &&
+      <MapView
+      style={styles.map} 
+      initialRegion={{
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005
+      }} 
+      />
+    }
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
